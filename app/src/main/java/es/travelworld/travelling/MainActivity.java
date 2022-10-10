8d98403e731cbe3b52f78a8103f9ca2e87576e5c
Alberto Rodríguez Díaz <choketido@gmail.com>
2022-10-10 03:28:32 +0200

    Creación de la aplicación Travelling

--- app/src/main/java/es/travelworld/travelling/MainActivity.java
+++ app/src/main/java/es/travelworld/travelling/MainActivity.java
@@ -2,13 +2,26 @@ package es.travelworld.travelling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.onboarding_layout);
+
+        TextView next = (TextView) findViewById(R.id.nextButton);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
