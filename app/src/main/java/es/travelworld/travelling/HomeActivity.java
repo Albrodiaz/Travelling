f94c32f448c6d846b431348fe1c38c9a54c679fb
Alberto Rodríguez Díaz <choketido@gmail.com>
2022-10-30 14:28:26 +0100

    Modificación de Archivo Themes y ajustes de código

--- app/src/main/java/es/travelworld/travelling/HomeActivity.java
+++ app/src/main/java/es/travelworld/travelling/HomeActivity.java
@@ -7,12 +7,10 @@ import static es.travelworld.travelling.Constants.KEY_USERPASWORD;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.snackbar.Snackbar;
