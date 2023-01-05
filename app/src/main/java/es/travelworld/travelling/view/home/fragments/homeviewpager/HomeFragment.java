482b61942035e2df65756507b6805654a20524ff
Albrodiaz <alrodiaz15@gmail.com>
2023-01-05 18:18:37 +0100

    Creación de Mapas de google y sus opciones (pendiente de modificar)

--- app/src/main/java/es/travelworld/travelling/view/home/fragments/homeviewpager/HomeFragment.java
+++ app/src/main/java/es/travelworld/travelling/view/home/fragments/homeviewpager/HomeFragment.java
@@ -1,19 +1,22 @@
 package es.travelworld.travelling.view.home.fragments.homeviewpager;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelStoreOwner;
-import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
@@ -24,9 +27,8 @@ import es.travelworld.travelling.repository.HotelsRespository;
 import es.travelworld.travelling.view.home.fragments.hotelsadapter.HotelsAdapter;
 import es.travelworld.travelling.view.home.viewmodels.HomeViewModel;
 import es.travelworld.travelling.view.home.viewmodels.HotelsViewModel;
-import es.travelworld.travelling.view.recyclerhome.VehicleAdapter;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentHomeBinding binding;
     private HomeViewModel homeViewModel;
@@ -51,6 +53,16 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initViewModels();
         observers();
+        initMap();
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance(mapOptions());
+        getParentFragmentManager()
+                .beginTransaction()
+                .add(R.id.map, mapFragment)
+                .commit();
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -65,7 +77,7 @@ public class HomeFragment extends Fragment {
 
     private void initViewModels() {
         homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
-        hotelViewModel = new ViewModelProvider((ViewModelStoreOwner) requireActivity(),
+        hotelViewModel = new ViewModelProvider(requireActivity(),
                 (ViewModelProvider.Factory) new HotelsViewModel.Factory(new HotelsRespository())).get(HotelsViewModel.class);
         hotelViewModel.loadHotels();
     }
@@ -74,4 +86,38 @@ public class HomeFragment extends Fragment {
         binding.hotelsRecyclerView.setHasFixedSize(true);
         binding.hotelsRecyclerView.setAdapter(new HotelsAdapter(list));
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng localizacion = new LatLng(37.21, -7.4043);
+        googleMap.addMarker(new MarkerOptions()
+                .position(localizacion)
+                .title("Posición aleatoria")
+                .snippet("Eooooo"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(localizacion));
+    }
+
+    private GoogleMapOptions mapOptions() {
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.zoomControlsEnabled(true);
+        options.zoomGesturesEnabled(true);
+        options.minZoomPreference(14);
+        return options;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
